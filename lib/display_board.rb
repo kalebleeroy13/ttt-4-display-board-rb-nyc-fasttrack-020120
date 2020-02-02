782bfc576b34faabbf1d27104dd4fd3a77e09d8d
kaleb hunter <khuunter@yahoo.com>
2020-02-02 14:38:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ puts board =[" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 puts center =[" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-def display_board(game)
+def display_board(game = board)
   puts " #{game[0]} | #{game[1]} | #{game[2]}   "
   puts "___________"
   puts " #{game[3]} | #{game[4]} | #{game[5]}   "
@@ -13,8 +13,8 @@ def display_board(game)
 end
 
 
-display_board(center)
+display_board()
 
-display_board(board)
+
 
 
