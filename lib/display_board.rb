2565b9c9ca03965d217ba1f84e36d2380ab74b07
kaleb hunter <khuunter@yahoo.com>
2020-02-02 15:17:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -24,9 +24,9 @@ puts xoxox = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 
 def display_board(game = board)
   puts " #{game[0]} | #{game[1]} | #{game[2]} "
-  puts "___________"
+  puts "-----------"
   puts " #{game[3]} | #{game[4]} | #{game[5]} "
-  puts "___________"
+  puts "-----------"
   puts " #{game[6]} | #{game[7]} | #{game[8]} "
 end
 
