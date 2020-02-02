7d66b93fd830cb60280e14e54c13c7cccbfdbd06
kaleb hunter <khuunter@yahoo.com>
2020-02-02 14:44:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-puts board =[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+puts board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 puts center =[" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-def display_board(board)
+def display_board(game = board)
   puts " #{game[0]} | #{game[1]} | #{game[2]}   "
   puts "___________"
   puts " #{game[3]} | #{game[4]} | #{game[5]}   "
